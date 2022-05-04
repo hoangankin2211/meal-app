8af8da5535f263ec8ec85caf2744531d6b802a1d
hoangankin2211 <73890707+hoangankin2211@users.noreply.github.com>
2022-05-04 10:10:55 +0700

    First commit_Initialization_[4/5/2022]

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyHomePage());
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meal App'),
+      ),
+    );
+  }
+}
